e87b7224089f8d63170d8c776ade02822fb7174c
Kalle Soukka <t9soka00@students.oamk.fi>
2020-03-26 12:07:54 +0200

    merged stuff together

--- application/views/browse/show_items.php
+++ application/views/browse/show_items.php
@@ -1,7 +1,9 @@
 <h2>Browse</h2>
 
 <?php 
+    $this->load->view('menu/header');
     foreach ($BrowseItems as $row) {
        echo $row['id_products'].$row['name'].$row['quantity'].$row['price'];
     } 
+    $this->load->view('menu/footer');
 ?>
