eed5b20e8c11218d677475cf6a6ba0a3b3c7cc9b
Henna Virnes <t9vihe00@students.oamk.fi>
2020-03-26 12:04:23 +0200

    Somehow it deleted my user_model over another user_model. I updated my part back in the file

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -22,4 +22,13 @@ class User_model extends CI_Model{
     $this->db->where('lastname',$given_username);
     return $this->db->get()->row('password');
   }
+  public function getUserInfo()
+  {
+      $id_user = 1; //This should indicate to the user who is currently logged in
+      $this->db->select('*');
+      $this->db->from('user_accounts');
+      $this->db->where('id_user',$id_user);
+      return $this->db->get()->result_array();
+  }
+
 }
