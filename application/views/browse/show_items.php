0d899408b26a1caaa6b4e9f4bf92a2b44dcadd3c
Kalle Soukka <t9soka00@students.oamk.fi>
2020-03-30 15:06:16 +0300

    started styling page

--- application/views/browse/show_items.php
+++ application/views/browse/show_items.php
@@ -1,9 +1,11 @@
 <h2>Browse</h2>
-
+<link rel="stylesheet" href="assets\css\style.css">
 <?php 
     $this->load->view('menu/header');
+    echo '<div id="content">' ;
     foreach ($BrowseItems as $row) {
-       echo '<img src="assets\Images\gold-toilet-paper.jpg" id="image">'.$row['name'].' '.$row['quantity'].' '.$row['price'].'€ ' ;
+       echo '<img src="assets\Images\gold-toilet-paper.jpg" id="image">'.$row['name'].' quantity: '.$row['quantity'].' '.$row['price'].'€' ;
     } 
+    echo '</div>';
     $this->load->view('menu/footer');
 ?>
