0bb2799f39726c5cc03278fc85af058379010f90
Kalle Soukka <t9soka00@students.oamk.fi>
2020-04-01 11:26:44 +0300

    small changes

--- application/views/browse/show_items.php
+++ application/views/browse/show_items.php
@@ -1,8 +1,7 @@
-<h2>Browse</h2>
 <link rel="stylesheet" href="assets\css\style.css">
 <?php 
     $this->load->view('menu/header');
-    echo '<div id="content">' ;
+    echo '<h2>Browse</h2>'.'<div id="content">' ;
     foreach ($BrowseItems as $row) {
        echo '<img src="assets\Images\gold-toilet-paper.jpg" id="image">'.$row['name'].' quantity: '.$row['quantity'].' '.$row['price'].'€' ;
     } 
