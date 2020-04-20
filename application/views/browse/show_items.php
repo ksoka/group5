98b652527949aaf1855b1ebc3c7b87b59c3c4d27
Kalle Soukka <t9soka00@students.oamk.fi>
2020-04-20 12:06:37 +0300

    browse styling done

--- application/views/browse/show_items.php
+++ application/views/browse/show_items.php
@@ -1,12 +1,12 @@
 <link rel="stylesheet" href="assets\css\style.css">
 <?php 
     $this->load->view('menu/header');
-    echo '<h2 id="world">The world of toilet paper</h2>'.'<div class="container">'.'<div class row row-cols-3' ;
+    echo '<h2 id="world">The world of toilet paper</h2>'.'<div id="content">' ;
     foreach ($BrowseItems as $row) {
-       echo '<img src="'.base_url('assets/images/').$row['image'].'" id="image"> <div id="items">'.$row['name'].' <br> '.$row['price'].'€ </div>'   ;
+       echo '<div id="content-items">.<img src="'.base_url('assets/images/').$row['image'].'" id="image"> <div id="items">'.$row['name'].' <br> '.$row['price'].'€ </div> </div>' ;
     }
-    echo '</div>'.'</div>'; 
+    echo '</div>'; 
     $this->load->view('menu/footer');
 ?>
 
-<a href="<?php echo site_url('Product') ?>">to the product</a>
+<a href="<?php echo site_url('Product') ?>">To the product</a>
